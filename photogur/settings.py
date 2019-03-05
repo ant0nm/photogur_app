72f074ff54a87c4383611b75032a51080b94d3e7
Anton <anton.mois33v@gmail.com>
2019-03-05 13:45:34 -0500

    Implemented a search form on every page of the web app

--- photogur/settings.py
+++ photogur/settings.py
@@ -118,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+# had to add this for the css file to load, idk why...
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
--- photogur/views.py
+++ photogur/views.py
@@ -13,3 +13,11 @@ def picture_show(request, id):
     context = {'picture': picture}
     html_string = render(request, 'picture.html', context)
     return HttpResponse(html_string)
+
+def picture_search(request):
+    # request.GET is a dictionary where keys are form filed names (that we assigned) and values are data entered by the user
+    query = request.GET['query']
+    search_results = Picture.objects.filter(artist=query)
+    context = {'pictures': search_results, 'query': query}
+    html_string = render(request, 'search.html', context)
+    return HttpResponse(html_string)
